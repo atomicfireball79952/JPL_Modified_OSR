c1b13ddbf1671f03b073927f9d61dc2b91f701ab
atomicfireball79952 <60675716+atomicfireball79952@users.noreply.github.com>
2020-02-24 10:15:40 -0500

    Delete launcher.sh

--- Rover_Code/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# launcher.sh
-# navigate to home directory, then to this directory, then execute python script, then back home
-
-cd /
-cd /home/pi/Rover_Code #where the script is
-sudo python Main.py #a commnad to run the script
-cd /
